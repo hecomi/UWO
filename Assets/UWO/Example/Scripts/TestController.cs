933cb737496e0ae5d0864594cbc0e94066a26a04
hecomi <info@hecomi.com>
2015-01-04 01:58:29 +0900

    fix player control method

--- Assets/UWO/Example/Scripts/TestController.cs
+++ Assets/UWO/Example/Scripts/TestController.cs
@@ -16,6 +16,8 @@ public class TestController : MonoBehaviour
 	public float moveSpeed = 1f;
 	public float rotationSpeed = 10f;
 	public float jumpForce = 3000f;
+	public int fireCoolDownFrame = 5;
+	private int fireCoolDownCount_ = 0;
 
 	private bool isGround_ = false;
 
@@ -33,50 +35,57 @@ public class TestController : MonoBehaviour
 	{
 		if (!isMainPlayer) return;
 
+		Move();
+		Rotate();
+		Jump();
+		Fire();
+	}
+
+	void Move()
+	{
 		var velocity = Vector3.zero;
+		velocity += Input.GetAxisRaw("Horizontal") * transform.right;
+		velocity += Input.GetAxisRaw("Vertical") * transform.forward;
+		velocity = velocity.normalized * moveSpeed;
+		if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+			velocity *= 1.5f;
+		}
+		transform.position += velocity * Time.deltaTime;
+	}
+
+	void Rotate()
+	{
 		var angularVelocity = 0f;
-
-		if (Input.GetKey(KeyCode.W)) {
-			velocity += transform.forward;
-		}
-		if (Input.GetKey(KeyCode.S)) {
-			velocity -= transform.forward;
-		}
-		if (Input.GetKey(KeyCode.A)) {
-			velocity -= transform.right;
-		}
-		if (Input.GetKey(KeyCode.D)) {
-			velocity += transform.right;
-		}
-
 		if (Input.GetKey(KeyCode.Q)) {
 			angularVelocity -= 1;
 		}
 		if (Input.GetKey(KeyCode.E)) {
 			angularVelocity += 1;
 		}
+		angularVelocity *= rotationSpeed;
+		transform.Rotate(Vector3.up, angularVelocity);
+	}
 
+	void Jump()
+	{
 		if (isGround_ && Input.GetKeyDown(KeyCode.Space)) {
 			isGround_ = false;
 			rigidbody.AddForce(Vector3.up * jumpForce);
 		}
-	
-		velocity = velocity.normalized * moveSpeed;
-		angularVelocity *= rotationSpeed;
+	}
 
-		transform.position += velocity * Time.deltaTime;
-		transform.Rotate(Vector3.up, angularVelocity);
-
-		if (Input.GetKeyDown(KeyCode.Return)) {
-			Destroy(gameObject);
-		}
-
+	void Fire()
+	{
 		if (Input.GetKey(KeyCode.F)) {
-			var forward = Camera.main.transform.forward;
-			var obj = Instantiate(bullet, transform.position + forward, bullet.transform.rotation) as GameObject;
-			obj.GetComponent<Rigidbody>().AddForce(forward * 3000);
-			Synchronizer.Instantiate(emitEffect, obj.transform.position, obj.transform.rotation);
+			if (fireCoolDownCount_ <= 0) {
+				fireCoolDownCount_ = fireCoolDownFrame;
+				var forward = Camera.main.transform.forward;
+				var obj = Instantiate(bullet, transform.position + forward, bullet.transform.rotation) as GameObject;
+				obj.GetComponent<Rigidbody>().AddForce(forward * 3000);
+				Synchronizer.Instantiate(emitEffect, obj.transform.position, obj.transform.rotation);
+			}
 		}
+		--fireCoolDownCount_;
 	}
 
 	void OnCollisionEnter(Collision collision)
